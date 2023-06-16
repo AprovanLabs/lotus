b6075676dc5b6307e3549765189921889445859e
WyattKadelbach <wkadelbach@gmail.com>
2023-06-16 14:35:34 -0500

    Added tourney title and date to home page

--- apps/tennis/src/pages/index.tsx
+++ apps/tennis/src/pages/index.tsx
@@ -4,14 +4,8 @@ import { ScreenSizeBreakpoints } from 'src/lib/style';
 import BasicLayout from '../layouts/BasicLayout';
 import Image from 'next/image';
 
-/**
- * Each folder under '/pages' matches up to a route on the website. The 'index.tsx' file within that folder is the code that runs for that page
- *
- * For example, this file is the code that runs for the home page (https://<tennis-site>.com/)
- */
 const HomePage = () => {
   return (
-    // We use 'layout' files to control how the page looks. This is a good place to put things like headers, footers, navigation, etc. This makes things appear consistent across the site
     <BasicLayout>
       <div
         className={css`
@@ -20,9 +14,10 @@ const HomePage = () => {
           display: flex;
           flex-direction: column;
           align-items: flex-start;
-          justify-content: center;
+          justify-content: flex-start;
           padding-left: 2rem;
           color: white;
+          margin-top: 5rem;
         `}
       >
         <div
@@ -32,28 +27,56 @@ const HomePage = () => {
             left: 0;
             bottom: 0;
             right: 0;
-            background: rgb(31,52,39);
-            background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(31,52,39,1) 60%), url('/resources/images/rtc-inside.jpeg');
+            background: rgb(31, 52, 39);
+            background: linear-gradient(
+              90deg,
+              rgba(255, 255, 255, 0) 31%,
+              rgba(31, 52, 39, 1) 66%
+            ),
+            url('/resources/images/rtc-inside.jpeg');
             background-size: cover;
             background-repeat: no-repeat;
             background-position: left;
           `}
         />
-        <h1 className="text-xl" style={{ backgroundColor: '#236494', marginBottom: '2rem', fontWeight: 800, whiteSpace: 'nowrap' }}>
-          Highly Strung Tennis Tournament
+        <h1
+          className={css`
+            position: relative;
+            z-index: 1;
+            background-color: transparent;
+            margin-bottom: 0;
+            font-size: 4.3rem;
+            font-weight: 200;
+            white-space: nowrap;
+          `}
+        >
+          Highly Strung
         </h1>
-        <Link href="/contact-us" style={{ backgroundColor: '#a4c36c', color: 'white', marginBottom: '1rem', fontWeight: 500 }}>
-          Contact Us
-        </Link>
-        <Link href="/tournaments" style={{ backgroundColor: '#a4c36c', color: 'white', marginBottom: '1rem', fontWeight: 500 }}>
-          Tournaments
-        </Link>
-        <Link href="/draws" style={{ backgroundColor: '#a4c36c', color: 'white', marginBottom: '1rem', fontWeight: 500 }}>
-          Draws
-        </Link>
-        <Link href="/sponsors" style={{ backgroundColor: '#a4c36c', color: 'white', marginBottom: '1rem', fontWeight: 500 }}>
-          Sponsors
-        </Link>
+        <h1
+          className={css`
+            position: relative;
+            z-index: 1;
+            background-color: transparent;
+            margin-top: 0.5rem;
+            font-size: 3.0rem;
+            font-weight: 200;
+            white-space: nowrap;
+          `}
+        >
+          Tennis Tournament
+          <span
+            className={css`
+              display: block;
+              font-size: 2.5rem;
+              font-weight: 400;
+              -webkit-text-fill-color: transparent;
+              -webkit-text-stroke-width: 1px;
+              -webkit-text-stroke-color: white;
+            `}
+          >
+            July 23rd - July 26th
+          </span>
+        </h1>
       </div>
     </BasicLayout>
   );
