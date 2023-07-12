b98457fb423a3801ad44324804dd346d29429af7
Jacob Sampson <jacob.samps@gmail.com>
2023-07-12 17:06:19 -0500

    Add SES integration

---
+++ apps/tennis/src/pages/api/submit-form.ts
@@ -0,0 +1,89 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
+import * as aws from "@aws-sdk/client-ses";
+import formidable from 'formidable';
+import { createReadStream } from 'fs';
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+// IMPORTANT: it needs to be placed outside the Function Handler
+const ses = new aws.SES({
+  apiVersion: '2010-12-01',
+  region: 'us-east-2', // Change it to match your region
+  credentials: {
+    accessKeyId: process.env.LT_AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.LT_AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+const transporter = nodemailer.createTransport({
+  SES: { ses, aws },
+});
+
+const sendEmail = (toEmail: string, {
+  name, email, phoneNumber, message,
+}: {
+  name: string,
+  email: string,
+  phoneNumber: string,
+  message: string,
+}, attachment?: { name: string; path: string }) => {
+  console.log('toEmail', toEmail, attachment)
+  return transporter.sendMail({
+    from: "noreply@highlystrung.tennis",
+    to: toEmail,
+    html: `
+          <h1>Highly Strung Tennis Submittino</h1>
+          <p>Name: ${name}</p>
+          <p>Email: ${email}</p>
+          <p>Phone Number: ${phoneNumber}</p>
+          <pre>${message}</pre>
+          `,
+    subject: "Highly Strung Tennis Submission",
+    attachments: attachment ? [{
+      filename: attachment?.name,
+      content: createReadStream(attachment?.path)
+    }] : [],
+  });
+};
+
+const submitForm = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send({
+      error: 'Method Not Allowed',
+    });
+  }
+
+  const form = formidable();
+
+  form.parse(req, (err, fields, files) => {
+    try {
+      console.log('fields', fields)
+      console.log('files', files)
+
+      const attachment = (files as any)?.file?.[0];
+
+      sendEmail(process.env.SEND_TO_EMAIL!, {
+        name: fields?.name?.[0],
+        email: fields?.email?.[0],
+        phoneNumber: fields?.phoneNumber?.[0],
+        message: fields?.message?.[0],
+      }, attachment ? {
+        path: attachment?.filepath,
+        name: attachment?.originalFilename ?? 'submission.pdf',
+      } : undefined);
+
+      res.status(200).redirect('/contact-us');
+    } catch (error) {
+      console.error(`❌ Failed to send email: `, error);
+
+      return res.status(400).redirect('/contact-us');
+    }
+  });
+};
+
+export default submitForm;
--- apps/tennis/src/pages/contact-us/index.tsx
+++ apps/tennis/src/pages/contact-us/index.tsx
@@ -3,41 +3,13 @@ import BasicLayout from 'src/layouts/BasicLayout';
 import { ContactModel } from 'src/lib/core/models/contact';
 import ContactService from 'src/lib/server/contact/contactService';
 
-const TO_EMAIL = 'jacobisamps+lotus@gmail.com';
-const SUBJECT = 'Tennis Tournament Entry';
-
 const ContactUsPage = ({ contact }: { contact: ContactModel }) => {
-  function handleFormSubmit(event: any) {
-    event.preventDefault();
-
-    const elements = event?.target?.elements;
-
-    if (elements == null) return;
-
-    const name = elements.name.value;
-    const email = elements.email.value;
-    const phone = elements.phone.value;
-    const message = elements.message.value;
-    console.log(name, email, phone, message);
-
-    const body = `
-    Name: ${name}
-
-    Email: ${email}
-
-    Phone Number: ${phone}
-
-    ${message}`;
-
-    window.open(`mailto:${TO_EMAIL}?subject=${SUBJECT}&body=${body}`);
-  }
-
   return (
     <BasicLayout contact={contact}>
       <div className="max-w-2xl mx-auto px-8 py-4 min-h-[70vh] w-full">
         <h1 className="text-xl mb-16 mt-4 text-center">Contact Us</h1>
 
-        <form onSubmit={handleFormSubmit}>
+        <form method="POST" action="/api/submit-form" encType="multipart/form-data">
           <div className="flex mb-2 flex-col w-full gap-4">
             <div className="grid weight-500">
               <label htmlFor="name">Name</label>
@@ -70,6 +42,13 @@ const ContactUsPage = ({ contact }: { contact: ContactModel }) => {
                 className="border-2 text-black rounded-sm border-black max-w-[12rem]"
               />
             </div>
+
+            <div className="grid weight-500">
+              <label htmlFor="phone" style={{ display: 'block', width: '100%' }}>
+                Attachment
+              </label>
+              <input type="file" name="file" />
+            </div>
           </div>
 
           <div className="grid weight-500 my-4">
@@ -80,7 +59,8 @@ const ContactUsPage = ({ contact }: { contact: ContactModel }) => {
               id="message"
               name="message"
               rows={2}
-              className="border-2 text-black rounded-sm border-black w-full max-w-sm"
+              defaultValue="Attached is my completed sign-up form for the upcoming tournament"
+              className="border-2 py-1 px-1 text-black rounded-sm border-black w-full max-w-sm"
             ></textarea>
           </div>
 
