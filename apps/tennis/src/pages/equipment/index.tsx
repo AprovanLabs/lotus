0fa3b8e1084f427920ec083df79fd20afacc6138
WyattKadelbach <wkadelbach@gmail.com>
2023-07-27 08:16:39 -0500

    Created and styled equipment page, began styling other page

--- apps/tennis/src/pages/equipment/index.tsx
+++ apps/tennis/src/pages/equipment/index.tsx
@@ -12,18 +12,185 @@ const EquipmentPage = ({ contact }: { contact: ContactModel }) => {
 
   return (
     <BasicLayout contact={contact}>
-      <h1
-        className="text-xl"
+      <div
         style={{
-          backgroundColor: '#236494',
+          backgroundColor: '#6c935c',
           color: 'white',
           marginBottom: '2rem',
           fontWeight: 800,
           whiteSpace: 'nowrap',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '1rem',
+          width: '100%',
         }}
       >
-        Equipment
-      </h1>
+        <h1 className="text-xl">Featured Items</h1>
+      </div>
+
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(4, 1fr)',
+          gap: '1rem',
+          minHeight: '100vh',
+          width: '100vw',
+        }}
+      >
+
+        {/* Box 1 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 1</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+
+        {/* Box 2 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 2</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+
+        {/* Box 3 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 3</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+
+        {/* Box 4 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 4</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+
+        {/* Box 5 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 5</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+
+        {/* Box 6 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 6</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+
+        {/* Box 7 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 7</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+
+        {/* Box 8 */}
+        <div
+          style={{
+            border: '2px solid black',
+            padding: '1rem',
+            minHeight: '50vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h2 style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Equipment 8</h2>
+          <Image src="/resources/images/tshirt.png" width={120} height={120} alt="Tennis Ball Icon" />
+          <p>Description of equipment</p>
+          <p style={{ fontSize: '0.8rem', color: 'gray' }}>$00.00</p>
+          <p style={{ fontSize: '0.7rem', color: 'gray' }}>out of stock</p>
+        </div>
+      </div>
     </BasicLayout>
   );
 };
@@ -39,3 +206,4 @@ export async function getStaticProps() {
 }
 
 export default EquipmentPage;
+
--- apps/tennis/src/pages/other-opportunities/index.tsx
+++ apps/tennis/src/pages/other-opportunities/index.tsx
@@ -5,25 +5,69 @@ import BasicLayout from 'src/layouts/BasicLayout';
 import Image from 'next/image';
 import ContactService from 'src/lib/server/contact/contactService';
 import { ContactModel } from 'src/lib/core/models/contact';
+import { ChevronDown } from 'lucide-react';
 
 const OtherPage = ({ contact }: { contact: ContactModel }) => {
   const router = useRouter();
-  const [isShowing, setIsShowing] = useState(false);
+
+  // State to keep track of whether each subheader is clicked or not
+  const [isDrillsOpen, setIsDrillsOpen] = useState(false);
+  const [isRacquetStringingOpen, setIsRacquetStringingOpen] = useState(false);
 
   return (
     <BasicLayout contact={contact}>
-      <h1
-        className="text-xl"
-        style={{
-          backgroundColor: '#236494',
-          color: 'white',
-          marginBottom: '2rem',
-          fontWeight: 800,
-          whiteSpace: 'nowrap',
-        }}
-      >
-        Other Opportunities
-      </h1>
+      <div>
+        <h1
+          className="text-xl"
+          style={{
+            backgroundColor: '#236494',
+            color: 'white',
+            marginBottom: '2rem',
+            fontWeight: 800,
+            whiteSpace: 'nowrap',
+          }}
+        >
+          Other Opportunities
+        </h1>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          {/* Drills Subheader */}
+          <div
+            style={{
+              marginBottom: '1rem',
+              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+            onClick={() => setIsDrillsOpen(!isDrillsOpen)}
+          >
+            <h2 style={{ fontWeight: 'bold', borderBottom: '1px solid #ccc', marginRight: '0.5rem' }}>
+              Drills
+            </h2>
+            <ChevronDown size={18} />
+          </div>
+          {isDrillsOpen && (
+            <p>Description of the Drills opportunity</p>
+          )}
+
+          {/* Racquet Stringing Subheader */}
+          <div
+            style={{
+              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+            onClick={() => setIsRacquetStringingOpen(!isRacquetStringingOpen)}
+          >
+            <h2 style={{ fontWeight: 'bold', borderBottom: '1px solid #ccc', marginRight: '0.5rem' }}>
+              Racquet Stringing
+            </h2>
+            <ChevronDown size={18} />
+          </div>
+          {isRacquetStringingOpen && (
+            <p>Description of the Racquet Stringing opportunity</p>
+          )}
+        </div>
+      </div>
     </BasicLayout>
   );
 };
@@ -39,3 +83,4 @@ export async function getStaticProps() {
 }
 
 export default OtherPage;
+
